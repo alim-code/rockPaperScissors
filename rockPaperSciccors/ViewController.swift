1fce1a0768a973e5e991db127aaedcb31f400170
Muhammad Ali <alim13@mcmaster.ca>
2021-12-22 14:38:27 -0500

    Initial Commit

---
+++ rockPaperSciccors/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rockPaperSciccors
+//
+//  Created by Muhammad Ali on 2021-12-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
