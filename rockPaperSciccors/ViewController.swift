633ead9eb875dd35d92d6b9ec90dee8546e9cb36
Muhammad Ali <alim13@mcmaster.ca>
2022-01-02 02:37:04 -0500

    final version of app

--- rockPaperSciccors/ViewController.swift
+++ rockPaperSciccors/ViewController.swift
@@ -7,13 +7,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class ViewController: UIViewController
+    {
+        let imagearray = [UIImage (named: "Rock"), UIImage (named: "Paper"), UIImage (named: "Scissors")]
+        
+        @IBOutlet weak var rockPaperScissorsImageView1: UIImageView!
+        @IBOutlet weak var rockPaperScissorsImageView2: UIImageView!
+        
+        @IBAction func Shoot(_ sender: UIButton)
+            {
+                rockPaperScissorsImageView1.image = imagearray [Int.random(in: 0...2)]
+                rockPaperScissorsImageView2.image = imagearray [Int.random(in: 0...2)]
+                
+                /* can also use rockPaperScissorsImageView1.image = imagearray.randomElement()*/
+            }
     }
-
-
-}
-
+    
