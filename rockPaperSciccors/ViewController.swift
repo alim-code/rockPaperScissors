25a0add4058f935b4a39c7624363f352e3b53140
Muhammad Ali <alim13@mcmaster.ca>
2022-01-06 05:14:39 -0500

    Revised version 5

--- rockPaperSciccors/ViewController.swift
+++ rockPaperSciccors/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController
     {
-        let imagearray = [UIImage (named: "rock"), UIImage (named: "paper"), UIImage (named: "scissors")]
+        let imagearray = [UIImage (named: "rock"), UIImage (named: "Paper"), UIImage (named: "Scissors")]
         
         @IBOutlet weak var rockPaperScissorsImageView1: UIImageView!
         @IBOutlet weak var rockPaperScissorsImageView2: UIImageView!
